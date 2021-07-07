5c1927b89244213dc6461aba3da80d20a3d8f3b4
Robert Forsström <robert@middleware.se>
2021-07-07 14:19:03 +0200

    changed config location

--- cmd/probear/main.go
+++ cmd/probear/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 
-	conf, err := ioutil.ReadFile("./config.yaml")
+	conf, err := ioutil.ReadFile("/config/probear.yaml")
 	if err != nil {
 		log.Fatalf("error loading config %s", err)
 	}
